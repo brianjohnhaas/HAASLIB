fc3370132a38af28ab79ffc8d125c44c76887a02
Brian Haas <brianjohnhaas@gmail.com>
2015-11-10 13:12:49 -0500

    update

--- test/test_UGER_task_arrayer.pl
+++ test/test_UGER_task_arrayer.pl
@@ -22,10 +22,22 @@ main: {
         
     }
     
-    $uger_runner->run(@cmds);
+
+    my $fail_cmd = "no_such_command this_should_fail";
+
+    print STDERR "** Initial submission of 11 jobs, 1 should fail\n";
+    $uger_runner->run(@cmds, $fail_cmd);
+
+    
+    print STDERR "** And submitting a second round... should just try the failed command this time.\n";
+    $uger_runner->run(@cmds, $fail_cmd);
 
 
-
+    print STDERR "** Now running with just the commands that succeded earlier, plus one more for fun.\n";
+    $uger_runner->run(@cmds, "echo one more for fun");
+    
+    print STDERR "\n\tDone testing.\n\n";
+    
     exit(0);
 }
 
