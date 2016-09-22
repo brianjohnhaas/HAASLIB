41a46b438bb1dcf5a06fff1eb3538db80fc2a043
Brian Haas <brianjohnhaas@gmail.com>
2016-09-22 15:53:22 -0400

    update

--- CTAT_fus_pipe_runner.pm
+++ CTAT_fus_pipe_runner.pm
@@ -40,7 +40,8 @@ sub run_fusion_pipe {
     ###################
     ## Run STAR-Fusion
     
-    my $cmd = "/home/unix/bhaas/GITHUB/CTAT_FUSIONS/STAR-Fusion/STAR-Fusion --left_fq $left_fq --right_fq $right_fq -O STAR-Fusion --genome_lib_dir $CTAT_GENOME_LIB";
+    #my $cmd = "/home/unix/bhaas/GITHUB/CTAT_FUSIONS/STAR-Fusion/STAR-Fusion --left_fq $left_fq --right_fq $right_fq -O STAR-Fusion --genome_lib_dir $CTAT_GENOME_LIB";
+    my $cmd = "/seq/regev_genome_portal/SOFTWARE/STAR-Fusion/STAR-Fusion_v0.7.1/STAR-Fusion/STAR-Fusion --left_fq $left_fq --right_fq $right_fq -O STAR-Fusion --genome_lib_dir $CTAT_GENOME_LIB";
     $pipeliner->add_commands( new Command($cmd, "star-fusion.ok"));
             
     $pipeliner->add_commands( new Command("gzip -c STAR-Fusion/Chimeric.out.junction > STAR-Fusion/Chimeric.out.junction.gz", "gzip_chimeric_out.ok"));
@@ -58,7 +59,8 @@ sub run_fusion_pipe {
     ######################
     ## Run FusionInspector
     
-    $cmd = "/home/unix/bhaas/GITHUB/CTAT_FUSIONS/FusionInspector/FusionInspector --left_fq $left_fq --right_fq $right_fq --out_dir FusionInspector --out_prefix finspector --prep_for_IGV --fusions STAR-Fusion/star-fusion.fusion_candidates.final.abridged.FFPM --genome_lib_dir $CTAT_GENOME_LIB --aligner_path /home/unix/bhaas/GITHUB/STAR/bin/Linux_x86_64_static/STAR "; 
+    #$cmd = "/home/unix/bhaas/GITHUB/CTAT_FUSIONS/FusionInspector/FusionInspector --left_fq $left_fq --right_fq $right_fq --out_dir FusionInspector --out_prefix finspector --prep_for_IGV --fusions STAR-Fusion/star-fusion.fusion_candidates.final.abridged.FFPM --genome_lib_dir $CTAT_GENOME_LIB --aligner_path /home/unix/bhaas/GITHUB/STAR/bin/Linux_x86_64_static/STAR "; 
+    $cmd = "/seq/regev_genome_portal/SOFTWARE/FUSION_INSPECTOR/FusionInspector_v0.7.0/FusionInspector/FusionInspector --left_fq $left_fq --right_fq $right_fq --out_dir FusionInspector --out_prefix finspector --prep_for_IGV --fusions STAR-Fusion/star-fusion.fusion_candidates.final.abridged.FFPM --genome_lib_dir $CTAT_GENOME_LIB --aligner_path /home/unix/bhaas/GITHUB/STAR/bin/Linux_x86_64_static/STAR "; 
     
     $pipeliner->add_commands( new Command($cmd, "FusionInspector.ok") );
     
@@ -78,22 +80,26 @@ sub run_fusion_pipe {
     
     
     
-     
-    $cmd = "/home/unix/bhaas/GITHUB/CTAT_FUSIONS/DISCASM/DISCASM --aligned_bam STAR-Fusion/Aligned.sortedByCoord.out.bam --chimeric_junctions STAR-Fusion/Chimeric.out.junction --left_fq $left_fq --right_fq $right_fq --out_dir DISCASM_OI_ASM --denovo_assembler Oases --normalize_reads";
-    $pipeliner->add_commands( new Command($cmd, "DISCASM.ok") );
 
-    $pipeliner->add_commands(new Command("gzip DISCASM_OI_ASM/oases_out_dir/oases.transcripts.fa", "gzip_discasm_fasta.ok"));
+    if (0) {
+        $cmd = "/home/unix/bhaas/GITHUB/CTAT_FUSIONS/DISCASM/DISCASM --aligned_bam STAR-Fusion/Aligned.sortedByCoord.out.bam --chimeric_junctions STAR-Fusion/Chimeric.out.junction --left_fq $left_fq --right_fq $right_fq --out_dir DISCASM_OI_ASM --denovo_assembler Oases --normalize_reads";
+        $pipeliner->add_commands( new Command($cmd, "DISCASM.ok") );
+        
+        $pipeliner->add_commands(new Command("gzip DISCASM_OI_ASM/oases_out_dir/oases.transcripts.fa", "gzip_discasm_fasta.ok"));
+        
+
     
-    #my $norm_left_fq = "DISCASM_OI_ASM/" . basename($left_fq) . ".extracted.fq.normalized_K25_C50_pctSD200.fq";
-    #my $norm_right_fq = "DISCASM_OI_ASM/" . basename($right_fq) . ".extracted.fq.normalized_K25_C50_pctSD200.fq";
-    
-    #$pipeliner->add_commands(new Command("gzip $norm_left_fq $norm_right_fq", "gzip_norm_fq.ok"));
-                
-    &append_capture_outputs_cmds($pipeliner, $capture_dir, ["DISCASM_OI_ASM/oases_out_dir/oases.transcripts.fa.gz",
-                                                            #"$norm_left_fq.gz",
-                                                            #"$norm_right_fq.gz"
-                                 ] );
-    
+        #my $norm_left_fq = "DISCASM_OI_ASM/" . basename($left_fq) . ".extracted.fq.normalized_K25_C50_pctSD200.fq";
+        #my $norm_right_fq = "DISCASM_OI_ASM/" . basename($right_fq) . ".extracted.fq.normalized_K25_C50_pctSD200.fq";
+        
+        #$pipeliner->add_commands(new Command("gzip $norm_left_fq $norm_right_fq", "gzip_norm_fq.ok"));
+        
+        &append_capture_outputs_cmds($pipeliner, $capture_dir, ["DISCASM_OI_ASM/oases_out_dir/oases.transcripts.fa.gz",
+                                                                #"$norm_left_fq.gz",
+                                                                #"$norm_right_fq.gz"
+                                     ] );
+        
+    }
     
 
     $pipeliner->run();
